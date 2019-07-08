14beeedb56a962282af35bf5db7cf13ec5a3cc94
anil1596 <anil.khadwal@gmail.com>
2019-07-09 00:00:57 +0530

    updated app

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-import classes from './Burger.module.css';
 
-const burger = (props) => {
-    //to get array of keys
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-        console.log('Outer loop');
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            console.log(igKey+"     "+i);
-            return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-    });
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+        .map( igKey => {
+            return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            } );
+        } )
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
